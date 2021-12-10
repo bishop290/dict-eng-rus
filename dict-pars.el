88d2149087834705b5fca2c9a92af225b79b6ab8
bishop <none>
2021-12-10 06:45:27 +0300

    Add search, first try

--- dict-pars.el
+++ dict-pars.el
@@ -10,6 +10,7 @@
 (defconst DICT-EN-RU-PATH (concat DICT-CURRENT-PATH "dictionary/korolew_enru.dwa"))
 (defconst DICT-RU-EN-PATH (concat DICT-CURRENT-PATH "dictionary/korolew_ruen.dwa"))
 (defconst DICT-TABLE-PATH (concat DICT-CURRENT-PATH "search-table"))
+(defconst DICT-MAX-WORD "99360")
 
 (setq DICT-TABLE (make-hash-table :test 'equal))
 (setq DICT-DICTIONARY nil)
@@ -89,10 +90,15 @@
 
 
 (defun dict-hashtable-add (list table)
-  (let ((current-list (split-string (car list) ":")))
+  (let ((current-list (split-string (car list) ":"))
+        (next-list (list "" DICT-MAX-WORD)))
+    (when (cdr list)
+      (setq next-list (split-string (cadr list) ":")))
     (puthash
      (car current-list)
-     (string-to-number (car (cdr current-list)))
+     (list
+      (string-to-number (cadr current-list))
+      (string-to-number (cadr next-list)))
      table)
     (when (cdr list)
       (dict-hashtable-add (cdr list) table))))
@@ -107,16 +113,47 @@
 
 (defun dict-load-dictionary ()
   (dict-file-to-hashtable DICT-TABLE-PATH DICT-TABLE)
-  (setq DICT-DICTIONARY (vconcat DICT-DICTIONARY (dict-file-to-list DICT-EN-RU-PATH)))
-  (setq DICT-DICTIONARY (vconcat DICT-DICTIONARY (dict-file-to-list DICT-RU-EN-PATH))))
+  (setq DICT-DICTIONARY (vconcat
+                         DICT-DICTIONARY
+                         (dict-file-to-list DICT-EN-RU-PATH)))
+  (setq DICT-DICTIONARY (vconcat
+                         DICT-DICTIONARY
+                         (dict-file-to-list DICT-RU-EN-PATH))))
+
+
+(defun dict-match (word string)
+  (string-match
+   (concat "^" word)
+   (downcase string)))
+
+
+(defun dict-search (word start-end-list)
+  (let ((indx (- (car start-end-list) 1))
+        (last-indx (- (cadr start-end-list) 1))
+        (current-string "")
+        (result-string ""))
+  (while (<= indx last-indx)
+    (setq current-string (elt DICT-DICTIONARY indx))
+    (when (dict-match word current-string)
+      (setq result-string
+            (concat result-string current-string "\n")))
+    (setq indx (+ indx 1)))
+  result-string))
 
 
 (defun dict-main ()
   (interactive)
-  (let ((word (downcase (dict-get-word))))
+  (let ((word (downcase (dict-get-word)))
+        (result-string ""))
     (if (equal word "")
         (message "=> Empty word! End.")
       (when (not DICT-DICTIONARY)
         (message "=> Load dictionary...")
         (dict-load-dictionary)))
+    (message "=> Start search")
+    (setq result-string
+          (dict-search
+           word
+           (gethash (substring word 0 2) DICT-TABLE)))
+    (message result-string)
     ))
